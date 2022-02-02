2bb56672db5a6d9102a0f8af41726404cdc6f115
KieceDonc <valentinverst.developer@gmail.com>
2022-02-02 15:28:35 +0100

    Added : Exercice 1 et 2

---
+++ TP1/Exo1/Exo1.js
@@ -0,0 +1,11 @@
+﻿function changeBackground() {
+    let colour = document.getElementsByName("couleur")[0].value
+    document.querySelectorAll("fieldset").forEach((element)=>{
+        element.style.backgroundColor = colour
+    })
+}
+
+function calculerSubvention(){
+    document.getElementsByName("subventionmax")[0].value = 
+    document.getElementsByName("nbmembres")[0].value*3
+}
